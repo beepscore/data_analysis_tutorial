6b93aa7c9268cc4e32ad95f392a1193b3805253e
beepscore <support@beepscore.com>
2017-09-24 17:53:21 -0700

    concatenate

--- data_analysis_tutorial/5_concatenate_append.py
+++ data_analysis_tutorial/5_concatenate_append.py
@@ -12,11 +12,13 @@ df1 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                     'US_GDP_Thousands': [50, 55, 65, 55]},
                    index=[2001, 2002, 2003, 2004])
 
+# similar to df1, index years are later
 df2 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                     'Int_rate': [2, 3, 2, 2],
                     'US_GDP_Thousands': [50, 55, 65, 55]},
                    index=[2005, 2006, 2007, 2008])
 
+# index years same as df1, columns are different
 df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                     'Int_rate': [2, 3, 2, 2],
                     'Low_tier_HPI': [50, 52, 50, 53]},
@@ -24,3 +26,5 @@ df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
 
 # concatenate
 
+d1_d2 = pd.concat([df1, df2])
+print(d1_d2)
