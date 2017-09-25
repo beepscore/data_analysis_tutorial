5bb4d919092f09b988b52f0b42b3b2720f55c63b
beepscore <support@beepscore.com>
2017-09-24 18:27:50 -0700

    Add merge info

--- data_analysis_tutorial/6_join_merge.py
+++ data_analysis_tutorial/6_join_merge.py
@@ -17,16 +17,20 @@ df2 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                     'US_GDP_Thousands': [50, 55, 65, 55]},
                    index=[2005, 2006, 2007, 2008])
 
+# Unemployment column is different from tutorial 5
 df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                     'Unemployment': [7, 8, 9, 6],
                     'Low_tier_HPI': [50, 52, 50, 53]},
                    index=[2001, 2002, 2003, 2004])
 
-# merge
+# merge - in this case merge is not ideal. It ignores index, duplicates some rows.
+# merge is most useful when importing database tables with a common key e.g. username
 d1_d3 = pd.merge(df1, df3, on='HPI')
 print(d1_d3)
 
-# this creates many NaN elements, join/merge may work better
+# merge multiple columns. this eliminates some duplication, but not all
+d1_d2 = pd.merge(df1, df2, on=['HPI', 'Int_rate'])
 print()
+print(d1_d2)
 
 # join
