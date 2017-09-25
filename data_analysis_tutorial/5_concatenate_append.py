00f0c6e1b61517f485dcfe944e346f0a10bbeae1
beepscore <support@beepscore.com>
2017-09-24 18:11:18 -0700

    Concatenate and append

--- data_analysis_tutorial/5_concatenate_append.py
+++ data_analysis_tutorial/5_concatenate_append.py
@@ -25,6 +25,28 @@ df3 = pd.DataFrame({'HPI': [80, 85, 88, 85],
                    index=[2001, 2002, 2003, 2004])
 
 # concatenate
-
 d1_d2 = pd.concat([df1, df2])
 print(d1_d2)
+
+# this creates many NaN elements, join/merge may work better
+d1_d2_d3 = pd.concat([df1, df2, df3])
+print()
+print(d1_d2_d3)
+
+# append- for these dataframes, append is similar to concatenate
+# Note dataframe append is not super efficient.
+# If you want to append frequently, it may be better to use a database.
+# Then pull from database into pandas dataframe for analysis/plotting
+df4 = df1.append(df2)
+print()
+print(df4)
+
+df5 = df1.append(df3)
+print()
+print(df5)
+
+# append new row, loses index
+s = pd.Series([80, 2, 50], index=['HPI', 'Int_rate', 'US_GDP_Thousands'])
+df6 = df1.append(s, ignore_index=True)
+print()
+print(df6)
