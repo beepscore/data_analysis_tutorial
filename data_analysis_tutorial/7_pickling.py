3ad38e5fda7bcc8c44e94779a571e750f2c3a9f4
beepscore <support@beepscore.com>
2017-09-25 12:42:29 -0700

    Extract function grab_initial_state_data().

--- data_analysis_tutorial/7_pickling.py
+++ data_analysis_tutorial/7_pickling.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pandas as pd
 from matplotlib import style
 import quandl
+import pickle
 
 style.use('ggplot')
 
@@ -51,37 +52,59 @@ def us_states_short():
     :return: short list of US states as 2 letter abbreviations e.g. ['ID', 'OR', 'WA']
     to avoid quandl api speed limit when looping requests
     """""
-    return ['ID', 'OR', 'WA']
+    return ['ID', 'MN', 'OR', 'WA']
 
 
-main_df = pd.DataFrame()
+def grab_initial_state_data():
+    """ 
+    serializes pandas dataframe object and writes to file states.pickle
+    text data can be saved to one of several text formats e.g. csv, json
+    However can't easily save a python object such as a trained machine learning classifier to a text file.
+    Instead, can serialize to a pickle.
+    pandas supports to and from pickle
+    """""
 
-for abbreviation in us_states_short():
-    query = "FMAC/HPI_" + str(abbreviation)
+    # use local variable to avoid calling function every loop iteration
+    states = us_states_short()
 
-    # df = quandl.get(query, authtoken=quandl_api_key)
-    # You have exceeded the API speed limit of 20 calls per 10 minutes. Please slow down your requests.
-    df = quandl.get(query)
+    main_df = pd.DataFrame()
 
-    # rename column 'Value' to abbreviation to avoid
-    # ValueError: columns overlap but no suffix specified: Index(['Value'], dtype='object')
+    for abbreviation in states:
 
-    df.rename(columns={'Value': str(abbreviation)}, inplace=True)
+        query = "FMAC/HPI_" + str(abbreviation)
 
-    if main_df.empty:
-        main_df = df
+        # You have exceeded the API speed limit of 20 calls per 10 minutes. Please slow down your requests.
+        # http://help.quandl.com/article/68-is-there-a-rate-limit-or-speed-limit-for-api-usage
+        # df = quandl.get(query)
+        # use authtoken to attempt to avoid speed limit
+        df = quandl.get(query, authtoken=quandl_api_key)
 
-    else:
-        main_df = main_df.join(df)
+        # rename column 'Value' to abbreviation to avoid
+        # ValueError: columns overlap but no suffix specified: Index(['Value'], dtype='object')
 
-print('main_df')
-print(main_df)
+        df.rename(columns={'Value': str(abbreviation)}, inplace=True)
 
-#                    OR          WA
-# Date
-# 1975-01-31   19.688651   17.478975
-# 1975-02-28   20.174137   17.544439
-# 1975-03-31   20.634533   17.652704
-# ...
-# 2017-06-30  213.900185  216.341550
-# [510 rows x 2 columns]
+        if main_df.empty:
+            main_df = df
+
+        else:
+            main_df = main_df.join(df)
+
+    print('main_df')
+    print(main_df)
+
+    #                    OR          WA
+    # Date
+    # 1975-01-31   19.688651   17.478975
+    # 1975-02-28   20.174137   17.544439
+    # 1975-03-31   20.634533   17.652704
+    # ...
+    # 2017-06-30  213.900185  216.341550
+    # [510 rows x 2 columns]
+
+    pickle_out = open('../data/output/states.pickle', 'wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
+
+
+grab_initial_state_data()
