122648f6cf4d64d1cc5d449908fb1eef92c1c5dc
beepscore <support@beepscore.com>
2017-09-30 20:42:07 -0700

    Replace code from web site with code from house_prices.py. Keep mortgage_30y

--- data_analysis_tutorial/13_join.py
+++ data_analysis_tutorial/13_join.py
@@ -1,5 +1,7 @@
 
+# Tutorial 13
 # https://pythonprogramming.net/joining-mortgage-rate-data-analysis-python-pandas-tutorial/
+# based on house_prices.py
 
 import quandl
 import pandas as pd
@@ -17,36 +19,121 @@ input_file_path = input_directory + input_file_name
 # rstrip to remove trailing \n
 quandl_api_key = open(input_file_path, 'r').read().rstrip()
 
+def us_states50():
+    """
+    :return: pandas series of 50 US states as 2 letter abbreviations e.g. ['AK', 'AL'...]
+    """
+    # https://pythonprogramming.net/dataset-data-analysis-python-pandas-tutorial
+    # us_states_list is a list of dataframes
+    us_states_list = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states', flavor='html5lib')
 
-def state_list():
-    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
-    return fiddy_states[0][0][1:]
+    # first dataframe
+    us_states_df = us_states_list[0]
+
+    abbreviation_series = us_states_df[0]
+    # print('abbreviation_series')
+    # print(abbreviation_series)
+
+    # slice row 1 to end (omit row0, contains series name 'Abbreviation')
+    # http://pandas.pydata.org/pandas-docs/stable/indexing.html#slicing-ranges
+    abbreviations = abbreviation_series[1:]
+    # print('abbreviations')
+    # print(abbreviations)
+
+    # python can iterate over a pandas series
+    # the series contains index and value, loop uses the value
+    return abbreviations
+
+
+def us_states_short():
+    """ 
+    :return: short list of US states as 2 letter abbreviations e.g. ['ID', 'OR', 'WA']
+    to avoid quandl api speed limit when looping requests
+    """
+    return ['ID', 'MN', 'OR', 'WA']
 
 
 def grab_initial_state_data():
-    states = state_list()
+    """ 
+    serializes pandas dataframe object and writes to file states.pickle
+    text data can be saved to one of several text formats e.g. csv, json
+    However can't easily save a python object such as a trained machine learning classifier to a text file.
+    Instead, can serialize to a pickle.
+    python has standard methods dump and load
+    Alternatively, can use pandas methods to_pickle and read_pickle
+    """""
+    # use local variable to avoid calling function every loop iteration
+    states = us_states_short()
 
     main_df = pd.DataFrame()
 
-    for abbv in states:
-        query = "FMAC/HPI_" + str(abbv)
+    for abbreviation in states:
+
+        query = "FMAC/HPI_" + str(abbreviation)
+
+        # You have exceeded the API speed limit of 20 calls per 10 minutes. Please slow down your requests.
+        # http://help.quandl.com/article/68-is-there-a-rate-limit-or-speed-limit-for-api-usage
+        # df = quandl.get(query)
+        # use authtoken to attempt to avoid speed limit
         df = quandl.get(query, authtoken=quandl_api_key)
-        print(query)
-        df[abbv] = (df[abbv] - df[abbv][0]) / df[abbv][0] * 100.0
-        print(df.head())
+
+        # rename column 'Value' to abbreviation to avoid
+        # ValueError: columns overlap but no suffix specified: Index(['Value'], dtype='object')
+        df.rename(columns={'Value': str(abbreviation)}, inplace=True)
+
+        # change state column values to percent change relative to row 0
+        # don't use pct_change(), it shows change from one point to next
+        df[abbreviation] = ((df[abbreviation] - df[abbreviation][0]) / df[abbreviation][0]) * 100
+
         if main_df.empty:
             main_df = df
+
         else:
             main_df = main_df.join(df)
 
-    pickle_out = open('fiddy_states3.pickle', 'wb')
+    print('main_df')
+    print(main_df)
+
+    # original state column values
+    #                    OR          WA
+    # Date
+    # 1975-01-31   19.688651   17.478975
+    # 1975-02-28   20.174137   17.544439
+    # 1975-03-31   20.634533   17.652704
+    # ...
+    # 2017-06-30  213.900185  216.341550
+    # [510 rows x 2 columns]
+
+    # state columns show percent change
+    #                     ID          MN          OR           WA
+    # Date
+    # 1975-01-31    0.000000    0.000000    0.000000     0.000000
+    # 1975-02-28   -0.156229    1.000795    2.465818     0.374531
+    # 1975-03-31   -0.324616    1.863569    4.804198     0.993931
+    # ...
+    # 2017-06-30  470.938021  497.672327  986.413619  1137.724456
+
+    # wb write bytes
+    pickle_out = open('../data/output/states_change.pickle', 'wb')
     pickle.dump(main_df, pickle_out)
     pickle_out.close()
 
+###
+# Tutorial 8 Percent Change and Correlation Tables
+# https://pythonprogramming.net/percent-change-correlation-data-analysis-python-pandas-tutorial/
 
-def HPI_Benchmark():
+def hpi_benchmark():
+    """
+    """
     df = quandl.get("FMAC/HPI_USA", authtoken=quandl_api_key)
-    df["United States"] = (df["United States"] - df["United States"][0]) / df["United States"][0] * 100.0
+    print(df)
+    us = 'United_States'
+
+    # rename column 'Value' to us to avoid
+    # ValueError: columns overlap but no suffix specified: Index(['Value'], dtype='object')
+    df.rename(columns={'Value': us}, inplace=True)
+
+    df[us] = ((df[us] - df[us][0]) / df[us][0]) * 100
     return df
 
 
@@ -56,3 +143,19 @@ def mortgage_30y():
     df = df.resample('1D')
     df = df.resample('M')
     return df
+
+
+# comment out after writing states.pickle
+# grab_initial_state_data()
+
+# read data from pickle
+# use python standard method
+# rb read bytes
+# pickle_in = open('../data/output/states.pickle', 'rb')
+# HPI_data = pickle.load(pickle_in)
+# print(HPI_data)
+
+# use pandas pickle methods, slightly shorter syntax
+# HPI_data.to_pickle('pickle.pickle')
+HPI_data = pd.read_pickle('../data/output/states_change.pickle')
+
