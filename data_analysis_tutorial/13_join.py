e220c5893cb3e18492dc6c4786f8f906e2f142e2
beepscore <support@beepscore.com>
2017-09-30 20:26:41 -0700

    Add 13_join.py, copied directly from https://pythonprogramming.net/joining-mortgage-rate-data-analysis-python-pandas-tutorial/. TODO: Update this code.

---
+++ data_analysis_tutorial/13_join.py
@@ -0,0 +1,53 @@
+
+# https://pythonprogramming.net/joining-mortgage-rate-data-analysis-python-pandas-tutorial/
+
+import Quandl
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+# Not necessary, I just do this so I do not show my API key.
+api_key = open('quandlapikey.txt', 'r').read()
+
+
+def state_list():
+    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddy_states[0][0][1:]
+
+
+def grab_initial_state_data():
+    states = state_list()
+
+    main_df = pd.DataFrame()
+
+    for abbv in states:
+        query = "FMAC/HPI_" + str(abbv)
+        df = Quandl.get(query, authtoken=api_key)
+        print(query)
+        df[abbv] = (df[abbv] - df[abbv][0]) / df[abbv][0] * 100.0
+        print(df.head())
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
+
+    pickle_out = open('fiddy_states3.pickle', 'wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
+
+
+def HPI_Benchmark():
+    df = Quandl.get("FMAC/HPI_USA", authtoken=api_key)
+    df["United States"] = (df["United States"] - df["United States"][0]) / df["United States"][0] * 100.0
+    return df
+
+
+def mortgage_30y():
+    df = Quandl.get("FMAC/MORTG", trim_start="1975-01-01", authtoken=api_key)
+    df["Value"] = (df["Value"] - df["Value"][0]) / df["Value"][0] * 100.0
+    df = df.resample('1D')
+    df = df.resample('M')
+    return df
