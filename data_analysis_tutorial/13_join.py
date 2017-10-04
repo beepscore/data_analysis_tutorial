fe51d6bf8087e095a1d95f17ad097cc32f9f396b
beepscore <support@beepscore.com>
2017-10-03 21:58:07 -0700

    Fix some methods. TODO: figure out join/merge

--- data_analysis_tutorial/13_join.py
+++ data_analysis_tutorial/13_join.py
@@ -138,8 +138,10 @@ def hpi_benchmark():
 
 
 def mortgage_30y():
-    df = quandl.get("FMAC/MORTG", trim_start="1975-01-01", authtoken=quandl_api_key)
+    #df = quandl.get("FMAC/MORTG", trim_start="1975-01-01", authtoken=quandl_api_key)
+    df = quandl.get("FMAC/MORTG", trim_start="1990-01-01", authtoken=quandl_api_key)
     df["Value"] = (df["Value"] - df["Value"][0]) / df["Value"][0] * 100.0
+    df.rename(columns={'Value': "m30"}, inplace=True)
 
     # fix warning
     # .resample() is now a deferred operation You called resample(...) on this deferred object
@@ -149,32 +151,119 @@ def mortgage_30y():
     df = df.resample('M').mean()
     return df
 
+# Tutorial 14 Adding other economic indicators
 
-def sp500_data():
-    df = quandl.get("YAHOO/INDEX_GSPC", trim_start="1975-01-01", authtoken=quandl_api_key)
-    df["Adjusted Close"] = (df["Adjusted Close"] - df["Adjusted Close"][0]) / df["Adjusted Close"][0] * 100.0
+
+# def sp500_data():
+#     """ This method doesn't work. quandl removed endpoint YAHOO/INDEX_GSPC
+#     # https://www.youtube.com/watch?list=PLQVvvaa0QuDc-3szzjeP6N6b0aDrrKyL-&time_continue=602&v=pxZy5jHID_A
+#     :return: dataframe
+#     """
+#     df = quandl.get("YAHOO/INDEX_GSPC", trim_start="1975-01-01", authtoken=quandl_api_key)
+#     df["Adjusted Close"] = (df["Adjusted Close"] - df["Adjusted Close"][0]) / df["Adjusted Close"][0] * 100.0
+#     df = df.resample('M').mean()
+#     df.rename(columns={'Adjusted Close': 'sp500'}, inplace=True)
+#     df = df['sp500']
+#     return df
+
+
+def sp500_data_from_file():
+    # https://www.youtube.com/watch?list=PLQVvvaa0QuDc-3szzjeP6N6b0aDrrKyL-&time_continue=602&v=pxZy5jHID_A
+    # https://finance.yahoo.com/quote/%5EGSPC/history?period1=157762800&period2=1506981600&interval=1mo&filter=history&frequency=1mo
+    # read the csv into a Dataframe
+    df = pd.read_csv('../data/input/sp500.csv')
+    # Convert the 'Date' column to a datetime object
+    df['Date'] = pd.to_datetime(df['Date'])
+    # set the index
+    df.set_index('Date', inplace=True)
+
+    # filter with a comparison operator. don't pass outliers
+    # df = df[(df['Date'] >= pd.to_datetime('1990-01-01'))]
+    # https://stackoverflow.com/questions/20233071/filter-pandas-dataframe-by-time-index#20233649
+    df = df.loc[df.index >= pd.to_datetime('1990-01-01')]
+
+    df["Adj Close"] = (df["Adj Close"]-df["Adj Close"][0]) / df["Adj Close"][0] * 100.0
     df = df.resample('M').mean()
-    df.rename(columns={'Adjusted Close': 'sp500'}, inplace=True)
+    df.rename(columns={'Adj Close': 'sp500'}, inplace=True)
     df = df['sp500']
+    df.columns = ['sp500']
     return df
 
 
-def gdp_data():
-    df = quandl.get("BCB/4385", trim_start="1975-01-01", authtoken=quandl_api_key)
-    df["Value"] = (df["Value"] - df["Value"][0]) / df["Value"][0] * 100.0
+def us_gdp_data():
+    """
+    gross domestic product
+    :return: dataframe
+    """
+    # brazil monthly gdb??
+    # https://www.quandl.com/data/BCB/4385-GDP-monthly-in-US-million
+    df = quandl.get("BCB/4385", trim_start="1990-01-01", authtoken=quandl_api_key)
+
+    df.rename(columns={'Value': 'gdp'}, inplace=True)
+    df["gdp"] = (df["gdp"] - df["gdp"][0]) / df["gdp"][0] * 100.0
     df = df.resample('M').mean()
-    df.rename(columns={'Value':'GDP'}, inplace=True)
-    df = df['GDP']
     return df
 
 
-def us_unemployment():
-    df = quandl.get("ECPI/JOB_G", trim_start="1975-01-01", authtoken=quandl_api_key)
-    df["Unemployment Rate"] = (df["Unemployment Rate"] - df["Unemployment Rate"][0]) / df["Unemployment Rate"][0] * 100.0
+def us_unemployment_data():
+    # quandl code used in tutorial doesn't work
+    # df = quandl.get("ECPI/JOB_G", trim_start="1975-01-01", authtoken=quandl_api_key)
+    # https://www.quandl.com/data/FRBC/UNEMP_ST_US-Unemployment-United-States
+    # df = quandl.get("FRBC/UNEMP_ST_US", trim_start="1975-01-01", authtoken=quandl_api_key)
+    df = quandl.get("FRBC/UNEMP_ST_US", trim_start="1990-01-01", authtoken=quandl_api_key)
+
+    df.rename(columns={'Value': 'unemp_rate'}, inplace=True)
+    df["unemp_rate"] = (df["unemp_rate"] - df["unemp_rate"][0]) / df["unemp_rate"][0] * 100.0
     df = df.resample('1D').mean()
     df = df.resample('M').mean()
     return df
 
+
+sp500 = sp500_data_from_file()
+print()
+print('sp500')
+# print(sp500.head())
+print(sp500)
+# sp500
+# Date
+# 1990-01-31      0.000000
+# 1990-02-28      0.853904
+# 1990-03-31      3.300114
+# ...
+# 2017-10-31    668.542670
+# Freq: M, Name: sp500, Length: 334, dtype: float64
+
+us_unemployment = us_unemployment_data()
+print()
+print('unemp_rate')
+# print(us_unemployment.head())
+print(us_unemployment)
+# us_unemployment
+#             unemp_rate
+# Date
+# 1990-01-31    0.000000
+# 1990-02-28   -1.851852
+# 1990-03-31   -3.703704
+# 1990-04-30    0.000000
+# ...
+# 2014-04-30   16.666667
+#
+# [292 rows x 1 columns]
+
+us_gdp = us_gdp_data()
+print()
+print('us_gdp')
+print(us_gdp)
+# us_gdp
+#                   gdp
+# Date
+# 1990-01-31   0.000000
+# 1990-02-28  -2.697505
+# 1990-03-31  -2.065278
+# ...
+# 2017-08-31  366.236401
+#
+# [332 rows x 1 columns]
 
 # comment out after writing states.pickle
 # grab_initial_state_data()
@@ -195,7 +284,8 @@ HPI_data = pickle.load(pickle_in)
 HPI_Bench = hpi_benchmark()
 print()
 print('HPI_Bench head')
-print(HPI_Bench.head())
+# print(HPI_Bench.head())
+print(HPI_Bench)
 #             United_States
 # Date
 # 1975-01-31       0.000000
@@ -203,28 +293,32 @@ print(HPI_Bench.head())
 # 1975-03-31       1.722313
 # 1975-04-30       3.106105
 # 1975-05-31       3.974628
+# ...
+# 2017-06-30     656.474194
+#
+# [510 rows x 1 columns]
 
-print()
+# print()
 m30 = mortgage_30y()
-m30.columns = ['M30']
+# m30.columns = ['m30']
 print('m30')
 print(m30)
 # m30
-#                   M30
+#                   m30
 # Date
-# 1975-01-31   0.000000
-# 1975-02-28  -3.393425
-# 1975-03-31  -5.620361
-# 1975-04-30  -6.468717
-# 1975-05-31  -5.514316
+# 1990-01-31   0.000000
+# 1990-02-28   3.030303
+# 1990-03-31   3.737374
 # ...
 # 2016-09-30 -63.308590
+#
+# [321 rows x 1 columns]
 
-HPI = HPI_Bench.join(m30)
-print()
-print('join')
-print(HPI.head())
-#             United_States       M30
+# HPI = HPI_Bench.join(m30)
+# print()
+# print('join')
+# print(HPI.head())
+#             United_States       m30
 # Date
 # 1975-01-31       0.000000  0.000000
 # 1975-02-28       0.657772 -3.393425
@@ -232,42 +326,42 @@ print(HPI.head())
 # 1975-04-30       3.106105 -6.468717
 # 1975-05-31       3.974628 -5.514316
 
-print()
-print('corr')
-print(HPI.corr())
+# print()
+# print('corr')
+# print(HPI.corr())
 # strong negative correlation- as mortgage interest rates increase hpi housing price index decreases
 # corr
-#                United_States       M30
+#                United_States       m30
 # United_States       1.000000 -0.778396
-# M30                -0.778396  1.000000
+# m30                -0.778396  1.000000
 
-state_HPI_M30 = HPI_data.join(m30)
-print('state_HPI_M30 corr')
-state_HPI_M30_corr = state_HPI_M30.corr()
-print(state_HPI_M30_corr)
-# state_HPI_M30 corr
-#            ID        MN        OR        WA       M30
+# state_HPI_m30 = HPI_data.join(m30)
+# print('state_HPI_m30 corr')
+# state_HPI_m30_corr = state_HPI_m30.corr()
+# print(state_HPI_m30_corr)
+# state_HPI_m30 corr
+#            ID        MN        OR        WA       m30
 # ID   1.000000  0.969281  0.994960  0.994941 -0.767792
 # MN   0.969281  1.000000  0.970385  0.973255 -0.765597
 # OR   0.994960  0.970385  1.000000  0.996621 -0.790943
 # WA   0.994941  0.973255  0.996621  1.000000 -0.791440
-# M30 -0.767792 -0.765597 -0.790943 -0.791440  1.000000
+# m30 -0.767792 -0.765597 -0.790943 -0.791440  1.000000
 
-print()
-print('select row index and last column')
-state_HPI_M30_corr_M30 = state_HPI_M30_corr['M30']
-print(state_HPI_M30_corr_M30)
+# print()
+# print('select row index and last column')
+# state_HPI_m30_corr_m30 = state_HPI_m30_corr['m30']
+# print(state_HPI_m30_corr_m30)
 # select row index and last column
 # ID    -0.767792
 # MN    -0.765597
 # OR    -0.790943
 # WA    -0.791440
-# M30    1.000000
-# Name: M30, dtype: float64
+# m30    1.000000
+# Name: m30, dtype: float64
 
 # tutorial showed this, but author and commenters noted probably should drop m30 from descriptive stats
-print()
-print(state_HPI_M30_corr_M30.describe())
+# print()
+# print(state_HPI_m30_corr_m30.describe())
 # count    5.000000
 # mean    -0.423154
 # std      0.795662
@@ -276,12 +370,12 @@ print(state_HPI_M30_corr_M30.describe())
 # 50%     -0.767792
 # 75%     -0.765597
 # max      1.000000
-# Name: M30, dtype: float64
+# Name: m30, dtype: float64
 
-print()
-print('drop m30 row')
-state_HPI_M30_corr_M30_drop_M30 = state_HPI_M30_corr_M30.drop(['M30'])
-print(state_HPI_M30_corr_M30_drop_M30.describe())
+# print()
+# print('drop m30 row')
+# state_HPI_m30_corr_m30_drop_m30 = state_HPI_m30_corr_m30.drop(['m30'])
+# print(state_HPI_m30_corr_m30_drop_m30.describe())
 # drop m30 row
 # count    4.000000
 # mean    -0.778943
@@ -291,4 +385,17 @@ print(state_HPI_M30_corr_M30_drop_M30.describe())
 # 50%     -0.779368
 # 75%     -0.767243
 # max     -0.765597
-# Name: M30, dtype: float64
+# Name: m30, dtype: float64
+
+# HPI_extended = HPI_data.join(m30, us_unemployment, us_gdp, sp500)
+# HPI_extended = HPI_data.join(m30, us_unemployment, sp500)
+# HPI_extended = HPI_data.join(m30, us_unemployment, how='left')
+# HPI_extended = HPI_data.join(m30, us_unemployment, sp500)
+# print()
+# print('HPI_extended')
+# print(HPI_extended)
+# HPI_extended_corr_drop_m30 = HPI_extended.corr().drop(['m30'])
+# print()
+# print('HPI_extended_corr_drop_m30')
+# print(HPI_extended_corr_drop_m30.describe())
+
